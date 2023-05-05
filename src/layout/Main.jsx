095787265faa82ebfed81aeb41e5dc11ef60e48f
unknown <tashfique.hcn@gmail.com>
2023-05-05 22:45:56 +0600

    spinner added

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import Header from '../Shared/Header/Header';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import Footer from '../Shared/Footer/Footer';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Main = () => {
+
+    const navigation = useNavigation();
+
     return (
         <div className='bg-slate-600'>
             <Header></Header>
+            <div>{navigation.state === 'loading' && 'Loading...'}</div>
             <Outlet></Outlet>
             <Footer></Footer>
             <ToastContainer></ToastContainer>
